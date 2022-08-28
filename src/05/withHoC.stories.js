1e15387177bf36a36646fd0be6b7e2bd4716c164
ysh88 <ysh88porcorosso@gmail.com>
2022-08-28 14:41:34 +0900

    p.245 개발자도구에서 확장컴포넌트의 이름으로 출력

--- src/05/withHoC.stories.js
+++ src/05/withHoC.stories.js
@@ -5,7 +5,14 @@ import withHoC from '../05/withHoC';
 
 export default {
   title: 'WithHoC',
+  component: withHoC,
+  args: {
+    children: '안녕하세요',
+  },
 };
+
+//1. 하이어오더 컴포넌트에 기존 컴포넌트 전달 => 기능이 추가된 확장 컴포넌트 반환
+//  withHoC(Button) === ButtonWithHoC = props => <Button {...props}>
 const ButtonWithHoC = withHoC(Button);
 const TextWithHoC = withHoC(Text);
 
@@ -17,15 +24,15 @@ const Template = args => {
     </>
   );
 };
+
+//2. 확장컴포넌트에 기존 props 전달
+//  ButtonWithHoc(props) => <Button {...props} />;
 export const Basic = Template.bind({});
-Basic.args = {
-  children: '안녕하세요',
-};
+Basic.args = {};
 Basic.storyName = '기본 설정';
 
 export const Large = Template.bind({});
 Large.args = {
   large: true,
-  children: '안녕하세요',
 };
 Large.storyName = 'large 예제';
