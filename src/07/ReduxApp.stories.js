2ed520eb1a0944052f11b2c334234e462782a1ab
ysh88 <ysh88porcorosso@gmail.com>
2022-08-31 22:55:20 +0900

    p.351 컴포넌트, 파일 이름 수정

--- src/07/ReduxApp.stories.js
+++ src/07/ReduxApp.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReduxApp from './ReduxApp';
+import ReduxApp1 from './ReduxApp1';
 import ReduxApp2 from './ReduxApp2';
 import ReduxApp3 from './ReduxApp3';
 import ADVReduxApp1 from './ADVReduxApp1'
@@ -9,20 +9,20 @@ export default {
   title: 'Redux App',
 };
 
-const Template1 = args => <ReduxApp {...args} />;
+const Template1 = args => <ReduxApp1 {...args} />;
 const Template2 = args => <ReduxApp2 {...args} />;
 const Template3 = args => <ReduxApp3 {...args} />;
 const Template4 = args => <ADVReduxApp1 {...args} />;
 const Template5 = args => <ADVReduxApp2 {...args} />;
 
 export const Basic = Template1.bind({});
-Basic.storyName = '기본 스토어 설정';
+Basic.storyName = 'ReduxApp1';
 
 export const Basic2 = Template2.bind({});
-Basic2.storyName = 'dispatch 예제';
+Basic2.storyName = 'ReduxApp2';
 
 export const Basic3 = Template3.bind({});
-Basic3.storyName = '기본 리듀서 예제';
+Basic3.storyName = 'ReduxApp3';
 
 export const Basic4 = Template4.bind({});
 Basic4.storyName = 'ADVReduxApp1';
