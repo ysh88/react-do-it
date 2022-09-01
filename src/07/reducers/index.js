c2036201fb2979b0e0b7a2e4eccfa6152d6533e2
ysh88 <ysh88porcorosso@gmail.com>
2022-09-02 00:36:20 +0900

    p381 최종 완료

--- src/07/reducers/index.js
+++ src/07/reducers/index.js
@@ -3,10 +3,14 @@ import user from './userReducer';
 import collection from './collectionReducer';
 import searchFilter from './searchFilterReducer';
 
-// 분리된 리듀서들 결합
 export default {
-  collection, // 리듀서 모듈 이름 = 분리된 리듀서들을 구별
+  collection,
   loading,
   user,
   searchFilter,
 };
+/* 
+combineReducers 함수에 인자로 전달 
+= 리듀서의 반환 결과를 저장할 객체의 키로 사용
+= 각 리듀서가 반환하는 값에 대응하는 키로 사용 = loading: true
+*/
--- src/07/reducers/searchFilterReducer.js
+++ src/07/reducers/searchFilterReducer.js
@@ -2,6 +2,8 @@ import { SET_FILTER, RESET_FILTER } from '../actions/searchFilterActions';
 
 const initState = {};
 
+// 리듀서를 호출할 때 전달되는 state인자는 리덕스에 있는 'global state' 전체가 아님
+// searchFilter영역의 state가 전달됨 => state.searchFilter
 export default function reducer(state = initState, action) {
   const { type, payload } = action;
 
@@ -9,7 +11,7 @@ export default function reducer(state = initState, action) {
     case SET_FILTER: {
       const { filterName, value } = payload;
       return {
-        ...state,
+        ...state, //state.searchFilter
         [filterName]: value,
       };
     }
